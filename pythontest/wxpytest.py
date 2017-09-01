241d876844f0b05989373f7fcabb34699b2e52b8
dfsd534 <dfsd534@qq.com>
2017-09-01 11:31:24 +0800

    some change

---
+++ pythontest/wxpytest.py
@@ -0,0 +1,6 @@
+from wxpy import *
+bot = Bot(cache_path = True)
+# my_friend = bot.friends().search('dfsd534')[0]
+# my_friend.send('hello world')
+myself = bot.self
+bot.file_helper.send('11111111111111')
